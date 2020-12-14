72876e5279223a6e81b414c79c5f87d62b873c28
Giovanni Londero <londero.giovanni@gmail.com>
2020-12-14 20:13:43 +0100

    Removed useless TODO

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.ViewModelProvider
 class ScoreViewModelFactory(private val finalScore: Int) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ScoreViewModel::class.java)) {
-            // TODO Construct and return the ScoreViewModel
             return ScoreViewModel(finalScore) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
